837618c073400a61ee2dbe73bed03d9a6ca710b0
hira22 <masayuki.hiraoka@ietty.co.jp>
2020-04-20 22:45:47 +0900

    Initial Commit

---
+++ UITabBarSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITabBarSample
+//
+//  Created by hiraoka on 2020/04/20.
+//  Copyright © 2020 hiraoka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
